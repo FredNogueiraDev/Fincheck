af4328b06b66f9ab0b656f95e286166d9fc44313
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-08-22 06:21:22 -0300

    feat: IsPublic e ActiveUserId decorators e módulos de Categories e BankAccounts

--- api/src/shared/database/repositories/users.repositories.ts
+++ api/src/shared/database/repositories/users.repositories.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { type Prisma } from '@prisma/client';
+
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class UsersRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(createUserDto: Prisma.UserCreateArgs) {
-    return this.prismaService.user.create(createUserDto);
+  create(createDto: Prisma.UserCreateArgs) {
+    return this.prismaService.user.create(createDto);
   }
 
   findUnique(findUniqueDto: Prisma.UserFindUniqueArgs) {
